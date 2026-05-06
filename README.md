c944247fe9404841781785b2c2d68f62307a071b
ailynb006 <pbecerracorvera@ucsd.edu>
2026-05-05 19:21:48 -0700

    added unit tests

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Ailyn Becerra
 ## Lab Links
 * [Part 1 - Expose](https://ailynb006.github.io/Lab5_Starter/expose.html)
 * [Part 2 - Explore](https://ailynb006.github.io/Lab5_Starter/explore.html)
+
+## Check for understanding questions
+1. I would not use a unit test to test the entire "message" feature of a message app since it would be able to test how sending or recieving a message will have changes reflected in the UI, the network, and the database all at the same time.
+2. I would use a unit test for the "max message length" feature since it is a specific feature that only checks if a string is over 80 characters and will not impact the network or UI.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,83 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+// Part 2
+/**
+ * isPhoneNumber Tests
+ */
+test('Validates standard phone number with parentheses', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('Validates standard phone number with dashes', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('Fails phone number with no formatting', () => {
+  expect(isPhoneNumber('1234567890')).toBe(false);
+});
+test('Fails phone number that is too short', () => {
+  expect(isPhoneNumber('123-456')).toBe(false);
+});
+
+/**
+* isEmail Tests
+*/
+test('Validates a standard .com email', () => {
+  expect(isEmail('hello@world.com')).toBe(true);
+});
+test('Validates an email with underscores and subdomains', () => {
+  expect(isEmail('my_name@ucsd.edu')).toBe(true);
+});
+test('Fails email missing the @ symbol', () => {
+  expect(isEmail('plainaddress.com')).toBe(false);
+});
+test('Fails email missing the top-level domain', () => {
+  expect(isEmail('test@gmail')).toBe(false);
+});
+
+/**
+* isStrongPassword Tests
+*/
+test('Validates a simple strong password', () => {
+  expect(isStrongPassword('Pass123')).toBe(true);
+});
+test('Validates a long password with underscores', () => {
+  expect(isStrongPassword('a_very_long_pw')).toBe(true);
+});
+test('Fails password starting with a number', () => {
+  expect(isStrongPassword('123Password')).toBe(false);
+});
+test('Fails password that is too short', () => {
+  expect(isStrongPassword('abc')).toBe(false);
+});
+
+/**
+* isDate Tests
+*/
+test('Validates date with double digit month/day', () => {
+  expect(isDate('12/25/2024')).toBe(true);
+});
+test('Validates date with single digit month/day', () => {
+  expect(isDate('1/1/2024')).toBe(true);
+});
+test('Fails date with 2-digit year', () => {
+  expect(isDate('12/25/24')).toBe(false);
+});
+test('Fails date with text instead of numbers', () => {
+  expect(isDate('Jan/01/2024')).toBe(false);
+});
+
+/**
+* isHexColor Tests
+*/
+test('Validates 6-character hex code', () => {
+  expect(isHexColor('#FF0000')).toBe(true);
+});
+test('Validates 3-character hex code', () => {
+  expect(isHexColor('#FFF')).toBe(true);
+});
+test('Fails hex code missing the # symbol', () => {
+  expect(isHexColor('#ZZZZZZ')).toBe(false);
+});
+test('Fails hex code with invalid characters (non-hex)', () => {
+  expect(isHexColor('#GGGGGG')).toBe(false);
+});
